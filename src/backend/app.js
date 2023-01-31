4d1bcb9ff215871ee4a124ac86b5cae368432992
cygap <113382267+Cygap@users.noreply.github.com>
2023-01-31 14:41:36 +0100

    implemented future-meal endpoint

--- src/backend/app.js
+++ src/backend/app.js
@@ -7,6 +7,7 @@ const mealsRouter = require("./api/meals");
 const buildPath = path.join(__dirname, "../../dist");
 const port = process.env.PORT || 3000;
 const cors = require("cors");
+const knex = require("./database");
 
 // For week4 no need to look into this!
 // Serve the built client html
@@ -20,11 +21,32 @@ app.use(express.json());
 app.use(cors());
 
 router.use("/meals", mealsRouter);
+//future-meals	Respond with all meals in the future (relative to the when datetime)
 
+app.get("/future-meal", async (req, res) => {
+  try {
+    const dbData = await knex.raw(
+      "Select * from Meal where Meal.when > CURRENT_TIMESTAMP()"
+    );
+    console.dir(dbData);
+    if (dbData[0].length) {
+      res.send(dbData[0]);
+    } else {
+      res.statusCode = 404;
+      res.send("No planned meals found for future ...");
+    }
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+//past-meals	Respond with all meals in the past (relative to the when datetime)
+//all-meals	Respond with all meals sorted by ID
+//first-meal	Respond with the first meal (meaning with the minimum id)
+//last-meal	Respond with the last meal (meaning with the maximum id)
 if (process.env.API_PATH) {
   app.use(process.env.API_PATH, router);
 } else {
-  throw "API_PATH is not set. Remember to set it in your .env file"
+  throw "API_PATH is not set. Remember to set it in your .env file";
 }
 
 // for the frontend. Will first be covered in the react class
